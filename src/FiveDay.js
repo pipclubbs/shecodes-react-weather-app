46ceb78a78a84cf4b78866111b72cf31b8f62ad3
pipclubbs <70511582+pipclubbs@users.noreply.github.com>
2021-07-28 13:56:32 +0100

    Update FiveDay.js

--- src/FiveDay.js
+++ src/FiveDay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FiveDay() {
-  return (
+    return (
     <div className="FiveDay">
       <div className="row">
         <div className="col-12">
